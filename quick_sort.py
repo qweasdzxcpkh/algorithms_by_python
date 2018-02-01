15e995b17469dffe35c5b919ceb67c8978355e96
潘浩贤 <carson.pan@herely.us>
2018-02-01 16:55:40 +0800

    feat: 快速排序实现的第二种方法

--- quick_sort.py
+++ quick_sort.py
@@ -6,32 +6,36 @@
 def quick_sort1(lists, left, right):
     '''快速排序'''
     if left >= right:
-        return lists
-    key = lists[left]
+        return
+    pivot = lists[left]
     low = left
     high = right
     while left < right:
-        while left < right and lists[right] >= key:
+        while left < right and lists[right] >= pivot:
             right -= 1
         lists[left] = lists[right]
-        while left < right and lists[left] <= key:
+        while left < right and lists[left] <= pivot:
             left += 1
         lists[right] = lists[left]
-    lists[right] = key
+    lists[right] = pivot
+    print lists  # 看排序过程
     quick_sort1(lists, low, left - 1)
     quick_sort1(lists, left + 1, high)
     return lists
 
 
 def quick_sort2(lists, left, right):
-    pass
-'''function partition(a, left, right, pivotIndex)
-     pivotValue := a[pivotIndex]
-     swap(a[pivotIndex], a[right]) // 把 pivot 移到結尾
-     storeIndex := left
-     for i from left to right-1
-         if a[i] < pivotValue
-             swap(a[storeIndex], a[i])
-             storeIndex := storeIndex + 1
-     swap(a[right], a[storeIndex]) // 把 pivot 移到它最後的地方
-     return storeIndex // 返回 pivot 的最终位置'''
+    '''快速排序2'''
+    if left >= right:
+        return
+    pivot = lists[right]
+    storeIndex = left
+    for i in range(left, right):
+        if lists[i] <= pivot:
+            lists[storeIndex], lists[i] = lists[i], lists[storeIndex]
+            storeIndex += 1
+    lists[storeIndex], lists[right] = lists[right], lists[storeIndex]
+    print lists  # 看排序过程
+    quick_sort2(lists, left, storeIndex-1)
+    quick_sort2(lists, storeIndex+1, right)
+    return lists
