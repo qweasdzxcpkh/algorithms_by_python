90323868065d814f69a23efef5ab078facd62e93
潘浩贤 <carson.pan@herely.us>
2018-02-26 00:40:35 +0800

    feat: 冒泡递归代码实现

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,3 +8,15 @@ def bubble_sort(lists):
             if lists[j] > lists[j+1]:
                 lists[j], lists[j+1] = lists[j+1], lists[j]
     return lists
+
+
+def bubble_sort_recursive(lists, left, right):
+    '''冒泡排序递归实现'''
+    if left == right:  # Base Case
+        return lists
+
+    for i in range(left, right):
+        if lists[i] > lists[i+1]:
+            lists[i], lists[i+1] = lists[i+1], lists[i]
+
+    return bubble_sort_recursive(lists, left, right-1)
